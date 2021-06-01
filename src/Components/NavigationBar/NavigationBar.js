b2d9006ae8712e749a485e690ca0be8e645f269f
Arifuzzaman-Nishan <arifuzzamannishan@gmail.com>
2021-06-02 05:16:45 +0600

    navbar sticky done

--- src/Components/NavigationBar/NavigationBar.js
+++ src/Components/NavigationBar/NavigationBar.js
@@ -10,24 +10,39 @@ import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 const NavigationBar = () => {
   return (
-    <div>
-      <Navbar className='nav-border' bg="light" expand="lg">
+    <div className="sticky">
+      <Navbar className="nav-border" bg="light" expand="lg">
         <Container>
           <Navbar.Brand href="#home">support@aleshamart.com</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link className='ml-2' href="#home">Download App</Nav.Link>
-              <Nav.Link className='ml-2' href="#link">Merchant Zone</Nav.Link>
-              <Nav.Link className='ml-2' href="#link">Become A Member</Nav.Link>
-              <Nav.Link className='ml-2' href="#link">Upload & Order</Nav.Link>
-              <Nav.Link className='ml-2' href="#link">Track Order</Nav.Link>
-              <Nav.Link className='ml-2' href="#link">Live Chat</Nav.Link>
-              <Nav.Link className='ml-2' href="#link">16731</Nav.Link>
+              <Nav.Link className="ml-2 " href="#home">
+                Download App
+              </Nav.Link>
+              <Nav.Link className="ml-2" href="#link">
+                Merchant Zone
+              </Nav.Link>
+              <Nav.Link className="ml-2" href="#link">
+                Become A Member
+              </Nav.Link>
+              <Nav.Link className="ml-2" href="#link">
+                Upload & Order
+              </Nav.Link>
+              <Nav.Link className="ml-2" href="#link">
+                Track Order
+              </Nav.Link>
+              <Nav.Link className="ml-2" href="#link">
+                Live Chat
+              </Nav.Link>
+              <Nav.Link className="ml-2" href="#link">
+                16731
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
+
       <Container className="">
         <Row className="d-flex align-items-center">
           <Col md={1}>
@@ -51,6 +66,7 @@ const NavigationBar = () => {
             </div>
           </Col>
         </Row>
+
         <Row className="category-border mt-2">
           <Col md={3}>
             <div className="d-flex align-items-center justify-content-between ">
@@ -58,14 +74,22 @@ const NavigationBar = () => {
               <FontAwesomeIcon className="arrow-icon" icon={faAngleDown} />
             </div>
           </Col>
-          <Col className='' md={9}>
+          <Col className="" md={9}>
             <Nav className="d-flex justify-content-end align-items-center">
-              <Nav.Link className='ml-4 nav-color' href="#home">CAMPAIGN</Nav.Link>
-              <Nav.Link className='ml-4 nav-color' href="#link">DISCOUNT</Nav.Link>
-              <FontAwesomeIcon className='icon-style ml-4' icon={faPlus}/>
-              <Nav.Link className='nav-color' href="#link">SIGN UP</Nav.Link>
-              <FontAwesomeIcon className='icon-style ml-4' icon={faUser}/>
-              <Nav.Link className='nav-color' href="#link">LOGIN</Nav.Link>
+              <Nav.Link className="ml-4 nav-color" href="#home">
+                CAMPAIGN
+              </Nav.Link>
+              <Nav.Link className="ml-4 nav-color" href="#link">
+                DISCOUNT
+              </Nav.Link>
+              <FontAwesomeIcon className="icon-style ml-4" icon={faPlus} />
+              <Nav.Link className="nav-color" href="#link">
+                SIGN UP
+              </Nav.Link>
+              <FontAwesomeIcon className="icon-style ml-4" icon={faUser} />
+              <Nav.Link className="nav-color" href="#link">
+                LOGIN
+              </Nav.Link>
             </Nav>
           </Col>
         </Row>
